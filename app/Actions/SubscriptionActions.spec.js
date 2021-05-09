10f5ccd41fc672239377e0bf04c38c1ba96a1dc9
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-09 18:12:48 +0300

    Testing structure has been created

---
+++ app/Actions/SubscriptionActions.spec.js
@@ -0,0 +1,12 @@
+import { onBeforeCreate } from "./SubscriptionActions";
+
+describe("onBeforeCreate", () => {
+  test("should be able to add timestamps", async () => {
+    const formData = {
+      name: "Ozzy",
+      created_at: null,
+    };
+    await onBeforeCreate({ formData });
+    expect(formData.created_at).not.toBe(null);
+  });
+});
