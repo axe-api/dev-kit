b2102571641a0e8ea91c5926d919c12f2f2d6bab
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-09 14:47:12 +0300

    ApiError example has been created

--- app/Actions/SubscriptionActions.js
+++ app/Actions/SubscriptionActions.js
@@ -1,6 +1,9 @@
+import { ApiError } from "axe-api";
+
 const onBeforeCreate = async ({ formData }) => {
   formData.created_at = new Date();
   formData.updated_at = new Date();
+  // throw new ApiError(406, "Olmadı beybi!");
 };
 
 export { onBeforeCreate };
