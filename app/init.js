8475a1c39a98b9e327a09b2c30b9d6b9f1aab3f8
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-16 16:16:07 +0300

    Added more example

--- app/init.js
+++ app/init.js
@@ -1,15 +1,11 @@
 export default async ({ app }) => {
-  app.get(
-    "/api/health",
-    (req, res, next) => {
-      res.json({
-        status: false,
-      });
-    },
-    (req, res) => {
-      res.json({
-        status: true,
-      });
-    }
-  );
+  app.use((req, res, next) => {
+    // Check something
+    next();
+  });
+  app.get("/api/health", (req, res) => {
+    res.json({
+      status: true,
+    });
+  });
 };
