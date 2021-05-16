a227807f96ab6ad70e431fb826ff694b05534bfc
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-16 16:41:29 +0300

    Fixed knexfile.js

--- app/Config/Database.js
+++ app/Config/Database.js
@@ -6,4 +6,11 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
   },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
 };
