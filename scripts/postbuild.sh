499c41592c4260dbe1b8bb84630180cb5c41855b
Ozgur Adem ISIKLI <ozgur@shape.dk>
2022-12-24 15:22:44 +0100

    Added some build and docker examples

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,4 @@
-cp package.json build/package.json
-cp .env build/.env 
-cp knexfile.js build/knexfile.js 
+cp package.json .env knexfile.js Dockerfile build/
 cp -R migrations build/migrations
 cd build 
 npm install --only=production
