8ef69803dbf20f3402a0e6f01edb388a929c2bbc
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-02-19 12:37:01 +0100

    Fixed hook/event folder structure

---
+++ dev-kit/app/v1/Hooks/User/onBeforeInsert.ts
@@ -0,0 +1,6 @@
+import { IoCService, IHookParameter } from "../../../../../index";
+
+export default async ({ formData }: IHookParameter) => {
+  // const mailer = await IoCService.use("Mailer");
+  // formData.created_at = new Date();
+};
