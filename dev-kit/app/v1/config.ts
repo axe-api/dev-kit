fa75af88fe90e0326c552cc62a1a55937187e702
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-03-15 18:46:32 +0100

    Added limit configs

--- dev-kit/app/v1/config.ts
+++ dev-kit/app/v1/config.ts
@@ -1,10 +1,18 @@
-import { IVersionConfig } from "../../../index";
+import { IVersionConfig, allow, QueryFeature } from "../../../index";
 
 const config: IVersionConfig = {
   transaction: [],
   serializers: [],
   supportedLanguages: ["en"],
   defaultLanguage: "en",
+  query: {
+    limits: [allow(QueryFeature.All)],
+    defaults: {
+      perPage: 10,
+      minPerPage: 5,
+      maxPerPage: 100,
+    },
+  },
 };
 
 export default config;
