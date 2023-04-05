f0c1d1efcb49463c27f2914252c92926fe8f89dc
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-04-06 00:07:42 +0300

    Fixed

--- dev-kit/app/config.ts
+++ dev-kit/app/config.ts
@@ -1,11 +1,10 @@
 import path from "path";
-import { LogLevels, IApplicationConfig, Frameworks } from "../../index";
+import { LogLevels, IApplicationConfig } from "../../index";
 
 const config: IApplicationConfig = {
   prefix: "api",
   env: process.env.NODE_ENV || "production",
   port: process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000,
-  framework: Frameworks.Express,
   logLevel: LogLevels.INFO,
   database: {
     client: process.env.DB_CLIENT || "mysql",
