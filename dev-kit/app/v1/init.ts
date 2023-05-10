eabd15aa959ea438c7ef8f463f2111114dfe2bd2
saracalihan <saracalihan@gmail.com>
2023-05-10 22:35:55 +0300

    default error handler added

--- dev-kit/app/v1/init.ts
+++ dev-kit/app/v1/init.ts
@@ -1,7 +1,11 @@
 import { Express } from "express";
+import ErrorHandler from "./Handlers/ErrorHandler.";
 
 const onBeforeInit = async (app: Express) => {};
 
-const onAfterInit = async (app: Express) => {};
+const onAfterInit = async (app: Express) => {
+  // Set global error handler.
+  app.use(ErrorHandler);
+};
 
 export { onBeforeInit, onAfterInit };
