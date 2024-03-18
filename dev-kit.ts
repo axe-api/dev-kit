ce1f0f34c7415cdd6964c2c34e7b33f7f6b21901
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-03-18 20:20:09 +0100

    Update dev-kit.ts

--- dev-kit.ts
+++ dev-kit.ts
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { Server } from "./index";
 
-logger.bgColor("yellow").color("black").log("Axe API dev-kit (1.0.1)");
-logger
-  .color("blue")
-  .log("Docs: https://axe-api.com/contribution/fundamentals.html \n");
-
+console.log("Axe API dev-kit (1.0.1)");
 dotenv.config();
 
 const server = new Server();
